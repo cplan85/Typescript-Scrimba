6bc4333f3d004a55eb37272bbe3164d2987b0aa1
cplan85 <carlos_planchart@live.com>
2022-05-26 18:51:20 +0200

    Up to Properties Challenge

---
+++ arrayTypesChallenge.ts
@@ -0,0 +1,70 @@
+// Array Types mini-challenge
+// Can you assign the correct Type to the reviews const? Please bear in mind everything
+// we have learnt about String, Boolean, Number, Object and Array Types for this.
+
+const returningUserDisplay = document.querySelector("#returning-user");
+const userNameDisplay = document.querySelector("#user");
+const reviewTotalDisplay = document.querySelector("#reviews");
+let isOpen: boolean;
+
+const reviews: {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
+}[] = [
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+  const iconDisplay = isLoyalty ? "⭐" : "";
+  reviewTotalDisplay.innerHTML =
+    "review total " +
+    value.toString() +
+    "| last reviewed by " +
+    reviewer +
+    " " +
+    iconDisplay;
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+const you: {
+  firstName: string;
+  lastName: string;
+  isReturning: boolean;
+  age: number;
+  stayedAt: string[];
+} = {
+  firstName: "Bobby",
+  lastName: "Brown",
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+};
+
+function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning == true) {
+    returningUserDisplay.innerHTML = "back";
+  }
+  userNameDisplay.innerHTML = userName;
+}
+
+populateUser(you.isReturning, you.firstName);
